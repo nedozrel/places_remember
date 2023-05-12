f046e76a83a032bc10fac818825fa3fa94911703
nedozrel <dixi.starodubtsev@gmail.com>
2023-05-12 09:05:43 +0700

    Fix place model

--- places_remember_app/models.py
+++ places_remember_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+# pylint: disable=imported-auth-user
 from django.contrib.auth.models import User
 
 
@@ -6,19 +7,17 @@ class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    # pylint: disable=too-few-public-methods
     class Meta:
         abstract = True
 
 
 class Place(BaseModel):
-    title = models.CharField(max_length=200, blank=True)
+    title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     longitude = models.DecimalField(max_digits=20, decimal_places=15)
     latitude = models.DecimalField(max_digits=20, decimal_places=15)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    # pylint: disable=too-few-public-methods
     class Meta:
         verbose_name = 'place'
         verbose_name_plural = 'places'
